883e3e6f60417f244cbf490c6e6c5bc4be1cbf15
andy <andy.lv.vip@hotmail.com>
2018-12-13 21:55:01 +0800

    correct the sql and tests...

--- sql/jpa.sql
+++ sql/jpa.sql
@@ -1,3 +1,3 @@
 create table user( id bigint not null auto_increment, first_name varchar(50) not null, last_name varchar(50) not null, address_id bigint, primary key(id) ) engine = InnoDB, charset = utf8;
 
-create table address( id bigint not null auto_increment, province vchar(20) not null, city vchar(20) not null, area vchar(20) not null, primary key(id) ) engine = InnoDB, charset = utf8;
+create table address( id bigint not null auto_increment, province varchar(20) not null, city varchar(20) not null, area varchar(20) not null, primary key(id) ) engine = InnoDB, charset = utf8;
--- src/test/java/aspire/demo/jpa/UserTests.java
+++ src/test/java/aspire/demo/jpa/UserTests.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -29,9 +30,9 @@ public class UserTests {
     }
 
     @Test
+    @Transactional
     public void findByFirstName() {
         List<User> users = userRepository.findByFirstName("leo");
-        Hibernate.initialize(users);
         System.out.println(users);
     }
 
